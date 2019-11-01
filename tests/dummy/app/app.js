3c9b54980c99c18fe0c0b12e1500059e85cdaf86
Moussa Saker <sak_moussa@yahoo.fr>
2019-11-01 01:18:55 +0100

    Adding a new ad below the header

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,21 +3,11 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
-});
-
-/* global window */
-Ember.Router.reopen({
-	notifyGoogleAnalytics: Ember.on('didTransition', function() {
-		if (!ga) { return; }
-		return ga('send', 'pageview', {
-			'page': this.get('url'),
-			'title': this.get('url')
-		});
-	})
 });
 
 loadInitializers(App, config.modulePrefix);
